1be729e7b102c854bf239ae1b1c50b1e8d48955c
mutiarapeggia <111202314924@mhs.dinus.ac.id>
2025-11-03 22:23:08 +0700

    Understanding JSX in ReactJS and ES6 (2)

--- src/Exercise.jsx
+++ src/Exercise.jsx
@@ -1,35 +1,34 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import UserCard from "./UserCard";
+import { getUsers } from "./Services";
 
 function Exercise() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getUsers();
+        setUsers(data);
+      } catch (error) {
+        console.error("[Componen] Gagal menampilkan data:", error.message);
+      }
+    };
+    fetchData();
+  }, []);
+
   return (
-    <>
-      <div className="min-h-screen bg-gray-100 p-6">
-        <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
-          User Cards
-        </h1>
-        <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
-          <UserCard
-            name="Mutia"
-            email="Mutiara@gmail.com"
-            street="Jl. Pucang Gding"
-            city="Semarang"
-          />
-          <UserCard
-            name="Peggia"
-            email="Peggia@gmail.com"
-            street="Jl. Pucang Anom"
-            city="Semarang"
-          />
-          <UserCard
-            name="Setiawan"
-            email="Setiawan@gmail.com"
-            street="Jl. Pucang Raya"
-            city="Semarang"
-          />
-        </div>
+    <div className="min-h-screen bg-gray-100 p-6">
+      <h1 className="text-3xl font-bold text-center mb-6 text-blue-700">
+        User Cards
+      </h1>
+
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6 max-w-5xl mx-auto">
+        {users.map((user) => (
+          <UserCard key={user.email} {...user} />
+        ))}
       </div>
-    </>
+    </div>
   );
 }
 
--- src/UserCard.jsx
+++ src/UserCard.jsx
@@ -2,12 +2,12 @@ import React, { useState } from "react";
 import { use } from "react";
 
 function Usecard(props) {
-  const { name, email, street, city } = props;
+  const { name, email, street, city, ...rest } = props;
   const [clicked, setClicked] = useState(false);
   // Fungsi handler
-  function handleClick() {
-    setClicked(true);
-  }
+  //function handleClick() {
+  //setClicked(true);
+  console.log(Object.entries(rest));
 
   return (
     <div className="bg-white p-6 rounded-lg shadow hover:shadow-md transition-shadow">
@@ -16,8 +16,17 @@ function Usecard(props) {
         <span className="font-medium">Email:</span> {email}
       </p>
       <p className="text-gray-600">
-        <span className="font-medium">Address:</span> {street},{city}
+        <span className="font-medium">Address:</span>
+        {street},{city}
       </p>
+
+      {/* Menampilkan data tambahan dari rest */}
+      {Object.entries(rest).map(([key, value]) => (
+        <p key={key} className="text-gray-600">
+          <span className="font-medium capitalize">{key}:</span> {value}
+        </p>
+      ))}
+
       <button
         className={`${
           clicked ? "bg-special-green" : "bg-gray-01"
